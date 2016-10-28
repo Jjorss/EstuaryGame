a448c59c429d9390c007bb0125ef1567a8812dbd
Jackson <jorss@udel.edu>
2016-10-27 22:21:50 -0400

    ok

--- src/tests/ClumpOfOystersTest.java
+++ src/tests/ClumpOfOystersTest.java
@@ -9,11 +9,11 @@ import model.ClumpOfOysters;
 public class ClumpOfOystersTest {
 	@Test
 	public void numberOfOystersTest() {
-		ClumpOfOysters tester = new ClumpOfOysters();
+		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
 		tester.spawn();
 		assertTrue(3 <= tester.getNumOfOystersInClump() || tester.getNumOfOystersInClump() <= 5);
 		
-		ClumpOfOysters tester2 = new ClumpOfOysters();
+		ClumpOfOysters tester2 = new ClumpOfOysters(0, 0);
 		tester2.spawn();
 		tester2.spawn();
 		tester2.spawn();
@@ -21,7 +21,7 @@ public class ClumpOfOystersTest {
 	}
 	@Test
 	public void moveTest() {
-		ClumpOfOysters tester = new ClumpOfOysters();
+		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
 		int oldX = tester.getX(); int oldY = tester.getY();
 		tester.move(); //doesn't move
 		assertEquals(tester.getX(), oldX);
@@ -29,7 +29,7 @@ public class ClumpOfOystersTest {
 	}
 	@Test(expected=UnsupportedOperationException.class)
 	public void exceptionTest() {
-		ClumpOfOysters tester = new ClumpOfOysters();
+		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
 	    tester.move();
 	}
 }
