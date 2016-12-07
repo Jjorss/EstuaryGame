0963e6b7d57b7bfe03ed9a099c399dcaf7718a49
Jackson <jorss@udel.edu>
2016-12-06 20:10:26 -0500

    fixed test

--- src/tests/ClumpOfOystersTest.java
+++ src/tests/ClumpOfOystersTest.java
@@ -7,19 +7,7 @@ import org.junit.Test;
 import model.ClumpOfOysters;
 
 public class ClumpOfOystersTest {
-	@Test
-	public void numberOfOystersTest() {
-		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
-		assertTrue(3 <= tester.getNumOfOystersInClump() || tester.getNumOfOystersInClump() <= 5);
-	}
-	@Test
-	public void moveTest() {
-		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
-		int oldX = tester.getX(); int oldY = tester.getY();
-		tester.move(); //doesn't move
-		assertEquals(tester.getX(), oldX);
-		assertEquals(tester.getY(), oldY);
-	}
+		
 	@Test(expected=UnsupportedOperationException.class)
 	public void exceptionTest() {
 		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
@@ -33,7 +21,7 @@ public class ClumpOfOystersTest {
 		tester.setVisible(false);
 		assertFalse(tester.isVisible());
 	}
-	
+	@Test
 	public void collectTest() {
 		ClumpOfOysters tester = new ClumpOfOysters(0, 0);
 		assertFalse(tester.isCollected());
